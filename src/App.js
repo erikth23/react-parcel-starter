70af60a001627fe1e1a26e5aaf1fb54a8104fb12
Erik Thomas-Hommer <erikth2355@gmail.com>
2021-04-15 09:59:56 -0700

    Added csv parsing for data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { csv } from 'd3-fetch';
 
 const viewHeight = 500;
 const viewWidth = 500;
 
 
 const App = () => {
+
+    useEffect(() => {
+      csv("https://raw.githubusercontent.com/erikth23/react-parcel-starter/main/generation_monthly.csv").then(function(data) {
+        console.log(data);
+      });
+    }, [])
+
     return (
-        <svg 
-            style={{border: "1px solid lightgrey", width: viewWidth,
-            height: viewHeight}}
-        >
-            <circle cx={20} cy={20} r="5" />
-            <rect x={200} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={212} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={224} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={236} y={200} width={10} height={10} fill="rgb(230, 230, 230)"/>
-            <rect x={248} y={200} width={10} height={10} />
-            <line x1={20} y1={viewHeight - 20} x2={150} y2={100} stroke="black" />
-            <text x="20" y="35">
-                Price history of 100 randomly selected Pokemon cards
-            </text>
-        </svg>
-    ); // outer brace = js / inner brace = object
+        <div>
+          <h1>Exploratory Data Analysis Assignment #2</h1>
+        </div>
+    );
 };
 
 export default App;
